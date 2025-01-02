a22a2d6fa63f4823861c5b1d41e1f1aa98d55c8a
samuuuu0 <138255054+samuuuu0@users.noreply.github.com>
2025-01-02 22:00:02 +0100

    set mongo port at 3001

--- solution/main-server/config/server.js
+++ solution/main-server/config/server.js
@@ -1,6 +1,6 @@
 const thisServer = 'http://localhost:3000'
 const springbootServer = 'http://localhost:8081'
-const mongodbServer = ''
+const mongodbServer = 'http://localhost:3001'
 
 module.exports = {
     thisServer,
