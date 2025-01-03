411b916405cc9738e688a3aab6a3cd49ffbef570
quelmitch <106555077+quelmitch@users.noreply.github.com>
2025-01-03 13:51:06 +0100

    Added module-alias for better path resolution

--- solution/main-server/routes/pages/movie-detail.js
+++ solution/main-server/routes/pages/movie-detail.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require("axios");
 const iso3166 = require('iso-3166-1');
-const { thisServer } = require('../../config/server')
+const { thisServer } = require('@config/server')
 const router = express.Router();
 
 router.get('/:id', (req, res) => {
@@ -11,15 +11,15 @@ router.get('/:id', (req, res) => {
 
     axios.get(`${thisServer}/api/v1/movie/${movie_id}`)
         .then((response) => {
+
+            // TODO fix some flag not displaying
             response.data.releases.forEach((release) => {
                 if (release.country) {
                     const countryCode = iso3166.whereCountry(release.country)?.alpha2;
                     if (countryCode) release.flagLink = `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
-                    console.log(release.flagLink)
                 }
             })
-
-
+            
             res.render('pages/movie-detail', {
                 title: response.data.title || 'Not Found',
                 movie: response.data
