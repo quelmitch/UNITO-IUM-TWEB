55d08af4de602de38e7bc5a8fb86ad065458b1f8
quelmitch <106555077+quelmitch@users.noreply.github.com>
2025-01-09 13:30:40 +0100

    Minor fixes & Api

---
+++ solution/main-server/public/scripts/movie-detail-rating.js
@@ -0,0 +1,32 @@
+const rating = 4.3; // Example rating
+const numStars = 5;
+const starContainer = document.getElementById('star-container');
+const ratingValue = document.getElementById('rating-value');
+
+// Generate stars based on rating
+for (let i = 0; i < numStars; i++) {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M12 2l2.39 7.26h7.61l-6.16 4.49 2.39 7.26-6.16-4.49-6.16 4.49 2.39-7.26-6.16-4.49h7.61z");
+    svg.appendChild(path);
+
+    const decimalPart = rating - Math.floor(rating);
+    const clipPercentage = decimalPart * 100;
+
+    if (rating >= i + 1) {
+        svg.classList.add('filled');
+    } else if (rating > i) {
+        svg.classList.add('half');
+        svg.style.clipPath = `inset(0 ${100 - clipPercentage}% 0 0)`;
+    } else {
+        svg.classList.add('star');
+    }
+
+    starContainer.appendChild(svg);
+}
+
+// Display the rating value
+ratingValue.textContent = `${rating.toFixed(1)}/5`;
--- solution/main-server/routes/api/v1/oscars/router.js
+++ solution/main-server/routes/api/v1/oscars/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require("axios");
 const { fromObjectToUri } = require('@routes-utils/common_service')
 const { springbootServer } = require('@config/server')
+const { groupByCeremonyAndCategory } = require("./service")
 
 const router = express.Router()
 
@@ -10,10 +11,12 @@ router.get('/filter', async (req, res) => {
 
     axios.get(`${springbootServer}/oscar/filter?${filters}`)
         .then((response) => {
-            res.json(response.data)
+            const data = groupByCeremonyAndCategory(response.data)
+            res.json(data)
         })
         .catch((error) => {
             // TODO
+            console.log(error)
             res.status(500).json({ error: 'Error communicating with the Spring Boot server' })
         })
 })
